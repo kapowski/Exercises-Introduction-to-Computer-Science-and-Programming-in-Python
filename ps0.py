dc150334612c4e9294bc443b23688d372464cc2c
jkantypowicz <32078611+jkantypowicz@users.noreply.github.com>
2022-10-10 14:43:23 -0700

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 10 14:15:29 2022
+
+@author: jorda
+"""
+#Practice Set 1 of INTRODUCTION TO COMPUTER SCIENCE AND PROGRAMMING IN PYTHON from OSSU
+
+import numpy as np
+
+x_input = input('Enter number x: ')
+x = int(x_input)
+y_input = input('Enter number y: ')
+y = int(y_input)
+x_y = x**y
+print('X**y = ',x_y)
+print ('log(x) = ',np.log2(x))
